1b8f9abb5f936994f818f31d09838febd91b025e
mikk22 <mikk.22@gmail.com>
2012-08-18 11:56:36 -0700

    first commit

---
+++ SRReposterTests/SRReposterTests.h
@@ -0,0 +1,13 @@
+//
+//  SRReposterTests.h
+//  SRReposterTests
+//
+//  Created by user on 18.08.12.
+//  Copyright (c) 2012 Intelvision. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SRReposterTests : SenTestCase
+
+@end
---
+++ SRReposterTests/SRReposterTests.m
@@ -0,0 +1,32 @@
+//
+//  SRReposterTests.m
+//  SRReposterTests
+//
+//  Created by user on 18.08.12.
+//  Copyright (c) 2012 Intelvision. All rights reserved.
+//
+
+#import "SRReposterTests.h"
+
+@implementation SRReposterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SRReposterTests");
+}
+
+@end
